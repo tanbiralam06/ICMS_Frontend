b5314e75124e88facc68775bb592602036db9b5f
Tanbir Alam <alamtanbir328@gmail.com>
2026-04-23 16:04:26 +0530

    fix invoice

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,17 +9,17 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-64 md:flex-col md:fixed md:inset-y-0 z-[80]">
+      <div className="hidden h-full md:flex md:w-64 md:flex-col md:fixed md:inset-y-0 z-[80] print:hidden">
         <Sidebar />
       </div>
-      <main className="md:pl-64 h-full">
+      <main className="md:pl-64 h-full print:pl-0">
         <div className="md:hidden print:hidden p-4 bg-white dark:bg-slate-900 border-b dark:border-slate-800 flex items-center shadow-sm sticky top-0 z-50">
           <MobileSidebar />
           <span className="font-bold text-lg text-slate-800 dark:text-slate-100 ml-2">
             BMS
           </span>
         </div>
-        <div className="p-8">{children}</div>
+        <div className="p-8 print:p-0">{children}</div>
       </main>
     </div>
   );
